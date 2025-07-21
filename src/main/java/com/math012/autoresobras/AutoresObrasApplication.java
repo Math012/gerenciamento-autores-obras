8ac4119e1563c99acfb0278fb1f692fb3ba41ae4
Math012 <math111.0811@gmail.com>
2025-07-21 15:29:30 -0300

    Commit inicial

---
+++ src/main/java/com/math012/autoresobras/AutoresObrasApplication.java
@@ -0,0 +1,13 @@
+package com.math012.autoresobras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoresObrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoresObrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/math012/autoresobras/AutoresObrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.math012.autoresobras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoresObrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
