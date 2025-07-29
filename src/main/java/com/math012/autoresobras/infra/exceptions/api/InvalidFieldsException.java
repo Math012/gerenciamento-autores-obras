d0c5b2580d48ba0a4dbc45c884519e5cbea78710
Math012 <math111.0811@gmail.com>
2025-07-29 06:11:29 -0300

    Criando a exceção InvalidFieldsException.

---
+++ src/main/java/com/math012/autoresobras/infra/exceptions/api/InvalidFieldsException.java
@@ -0,0 +1,7 @@
+package com.math012.autoresobras.infra.exceptions.api;
+
+public class InvalidFieldsException extends RuntimeException {
+    public InvalidFieldsException(String message) {
+        super(message);
+    }
+}
