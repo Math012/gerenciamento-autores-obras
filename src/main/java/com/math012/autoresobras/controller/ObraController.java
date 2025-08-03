1903e0b51f7346c62574d2d976b4e51d93aeaaae
Math012 <math111.0811@gmail.com>
2025-08-03 04:53:55 -0300

    Documentando a classe ObraController com o Swagger

--- src/main/java/com/math012/autoresobras/controller/ObraController.java
+++ src/main/java/com/math012/autoresobras/controller/ObraController.java
@@ -3,6 +3,8 @@ package com.math012.autoresobras.controller;
 import com.math012.autoresobras.business.DTO.request.ObraRequestDTO;
 import com.math012.autoresobras.business.DTO.response.ObraResponseDTO;
 import com.math012.autoresobras.business.service.ObraService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,11 +21,19 @@ public class ObraController {
     private final ObraService service;
 
     @PostMapping("/create")
+    @Operation(summary = "Endpoint para criar uma obra", description = "Criando uma obra")
+    @ApiResponse(responseCode = "200", description = "Obra criado com sucesso")
+    @ApiResponse(responseCode = "400", description = "Erro ao criar a obra, campos inválidos")
+    @ApiResponse(responseCode = "401", description = "Falha na autenticação")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<ObraResponseDTO> createAuthorsWorks(@RequestBody ObraRequestDTO obraRequestDTO) {
         return ResponseEntity.ok(service.createAuthorsWorks(obraRequestDTO));
     }
 
     @GetMapping("/page/all")
+    @Operation(summary = "Endpoint para listar todas as obras", description = "Listando todas as obras com paginação")
+    @ApiResponse(responseCode = "200", description = "Obras encontradas com sucesso")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Map<String, Object>> getAllAuthorsWorksWithPage(
             @RequestParam(defaultValue = "0", required = false) int page,
             @RequestParam(defaultValue = "5", required = false) int size
@@ -32,16 +42,31 @@ public class ObraController {
     }
 
     @GetMapping("/name")
+    @Operation(summary = "Endpoint para encontrar obras através do nome do autor", description = "Encontrando todas obras com base no nome do autor")
+    @ApiResponse(responseCode = "200", description = "Obra encontrada com sucesso")
+    @ApiResponse(responseCode = "401", description = "Falha na autenticação")
+    @ApiResponse(responseCode = "404", description = "Erro ao encontrar a obra, autor não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<List<ObraResponseDTO>> findAllAuthorsWorksByName(@RequestParam("author") String author) {
         return ResponseEntity.ok(service.findAllAuthorsWorksByName(author));
     }
 
     @PutMapping("/update/{id}")
+    @Operation(summary = "Endpoint para atualizar obra através do id", description = "Atualizando obra com base no id")
+    @ApiResponse(responseCode = "200", description = "Obra atualizada com sucesso")
+    @ApiResponse(responseCode = "401", description = "Falha na autenticação")
+    @ApiResponse(responseCode = "404", description = "Erro ao atualizar a obra, id não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<ObraResponseDTO> updateAuthorsWorks(@PathVariable("id") Long id, @RequestBody ObraRequestDTO obraRequestDTO) {
         return ResponseEntity.ok(service.updateAuthorsWorks(id, obraRequestDTO));
     }
 
     @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Endpoint para deletar obra através do id", description = "Deletando obra com base no id")
+    @ApiResponse(responseCode = "200", description = "Obra deletada com sucesso")
+    @ApiResponse(responseCode = "401", description = "Falha na autenticação")
+    @ApiResponse(responseCode = "404", description = "Erro ao deletar a obra, id não encontrado")
+    @ApiResponse(responseCode = "500", description = "Erro no servidor")
     public ResponseEntity<Void> deleteAuthorsWorksById(@PathVariable("id") Long id) {
         service.deleteAuthorsWorksById(id);
         return ResponseEntity.ok().build();
